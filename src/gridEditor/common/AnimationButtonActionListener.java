8ead86a67b284d1e2ea04916d9bea5b11391bbfa
RMSATS <RobertMatthewStewart@gmail.com>
2017-04-13 14:49:47 -0700

    Added animation functionalily with play, pause, and stop. Added new .tan file with 'I' animation. Other small changes.

---
+++ src/gridEditor/common/AnimationButtonActionListener.java
@@ -0,0 +1,44 @@
+package gridEditor.common;
+
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class AnimationButtonActionListener implements MouseListener{
+	
+	public AnimationButtonActionListener(){
+		// Blank on purpose
+	}
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// Auto-generated method stub
+		
+	}
+	
+
+}
